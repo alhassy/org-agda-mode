4b0bb02e734a8d1f984f1bf55ce5fb9648cb30c7
Mark Armstrong <armstmp@mcmaster.ca>
2019-03-16 01:17:48 -0400

    Rewrite the rewrite-ends function using lazy regular expressions.

--- literate.el
+++ literate.el
@@ -1,37 +1,31 @@
 ;; This file is generated from literate.lagda.
 
-;; “The long lost Emacs string manipulation library”
-;; https://github.com/magnars/s.el
-(require 's)
-
-(defun strip (pre post it)  
-  "A simple extraction: it = ⟨pre⟩it₀⟨post⟩ ↦ it₀." 
-  (s-chop-prefix pre (s-chop-suffix post it)) )
-
-(defun rewrite-ends (pre post newPre newPost)
+(defun rewrite-ends (pre post new-pre new-post)
   "Perform the following in-buffer rewrite: ⟨pre⟩⋯⟨post⟩ ↦ ⟨newPre⟩⋯⟨newPost⟩.
   For example, for rewriting begin-end code blocks from Org-mode to something
   else, say a language's default literate mode.
 
-  Warning: The body, the “⋯”, cannot contain the `#` character.
-  I do this so that the search does not go to the very last occurence of `#+END_SRC`;
-  which is my primary instance of `pre`.
+  The search for the string ⟨pre⟩⋯⟨post⟩ is non-greedy, i.e. will find
+  (in order) the minimal strings matching ⟨pre⟩⋯⟨post⟩.
 
   In the arguments, only symbol `\` needs to be escaped.
-
-  Implementation: Match the pre, then any characteer that is not `#`, then the post.
-  Hence, the body cannot contain a `#` character!
-  In Agda this is not an issue, since we can use its Unicode cousin `♯` instead.
   "
-  (let* ((rxPre     (regexp-quote pre))
-         (rxPost    (regexp-quote post))
-         (altered (replace-regexp-in-string (concat rxPre "\\([^\\#]\\|\n\\)*" rxPost)
-                  (lambda (x) (concat newPre (strip pre post x) newPost))
-                  (buffer-string) 'no-fixed-case 'new-text-is-literal)))
-      (erase-buffer)
-      (insert altered)
-   )
-)
+  (let ((rx-pre  (concat "\\(" (regexp-quote pre)  "\\)"))
+        (rx-post (concat "\\(" (regexp-quote post) "\\)"))
+        ;; Code to match any characters (including newlines) based on https://www.emacswiki.org/emacs/MultilineRegexp
+        ;; This version requires we end in a newline,
+        ;; and uses the “non-greedy” * operator, *?, so we will match the minimal string.
+        (body "\\(.*\n\\)*?"))
+    (beginning-of-buffer)
+    (while (re-search-forward (concat rx-pre body rx-post) nil t) ;; nil to search whole buffer, t to not error
+      ;; Matched string 1 is the pre, matched string 3 is the post.
+      ;; Optionals: fixed-case, literal, use buffer, substring
+      (replace-match new-pre  t t nil 1)
+      (replace-match new-post t t nil 3)
+      )
+    )
+  )
+
 
 (defun lagda-to-org ()
   "Transform literate Agda blocks into Org-mode source blocks.
