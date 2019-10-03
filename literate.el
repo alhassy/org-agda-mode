ffe3e9fc7519a0b5b2d140f8a7eeb9fbbb6e3339
Musa Al-hassy <alhassy@gmail.com>
2019-10-03 07:58:27 -0400

    Radical improvement … I know some Lisp now lol.

--- literate.el
+++ literate.el
@@ -1,85 +1,19 @@
-;; This file is generated from literate.lagda.
+;; [[file:~/org-agda-mode/literate.lagda][No heading:1]]
+;; Dependencies
+(use-package s :demand t)
+(use-package dash :demand t)
+;; No heading:1 ends here
 
+;; [[file:~/org-agda-mode/literate.lagda::*Agda%20Syntax%20Highlighting][Agda Syntax Highlighting:1]]
 ;; To use generic-mode later below.
 (require 'generic-x)
+;; Agda Syntax Highlighting:1 ends here
 
+;; [[file:~/org-agda-mode/literate.lagda::*Agda%20Syntax%20Highlighting][Agda Syntax Highlighting:2]]
 (add-to-list 'org-src-lang-modes '("agda" . ob-agda))
+;; Agda Syntax Highlighting:2 ends here
 
-(defface agda2-highlight-keyword-face
-  '((t (:foreground "DarkOrange3")))
-  "The face used for keywords."
-    :group 'font-lock-faces)
-
-(setq font-lock-keyword-face 'agda2-highlight-keyword-face)
-
-(defface agda2-highlight-symbol-face
-  '((((background light)) (:foreground "gray25"))
-    (((background dark))  (:foreground "gray75")))
-  "The face used for symbols like forall, =, as, ->, etc."
-  :group 'font-lock-faces)
-
-(setq org-agda-keywords
-  '("=" "|" "->" "→" ":" "?" "\\" "λ" "∀" ".." "..." "abstract" "codata"
-  "coinductive" "constructor" "data" "do" "eta-equality" "field"
-  "forall" "hiding" "import" "in" "inductive" "infix" "infixl"
-  "infixr" "instance" "let" "macro" "module" "mutual" "no-eta-equality"
-  "open" "overlap" "pattern" "postulate" "primitive" "private" "public"
-  "quote" "quoteContext" "quoteGoal" "quoteTerm" "record" "renaming"
-  "rewrite" "Set" "syntax" "tactic" "unquote" "unquoteDecl" "unquoteDef"
-  "using" "where" "with"))
-
-; (defvar org-agda-extra-word-colours nil "other words that user of org-mode wants coloured, along with their specified font-lock-type-face")
-
-(define-generic-mode
-
-    'ob-agda-mode                      ;; name of the mode
-
-    (list '("{-" . "-}"))               ;; comments delimiter
-
-    org-agda-keywords
-
-    ;; font lock list: Order of colouring matters;
-    ;; the numbers refer to the subpart, or the whole(0), that should be coloured.
-
-    (-concat  ;; ★★★★★★★★★★★★★★ org-agda-extra-word-colours is a free variable, user should define it /before/ loading org-agda-mode ★★★★★★★★★★★★★★
-	       (if (boundp (quote org-agda-extra-word-colours)) org-agda-extra-word-colours nil)
-    (list
-
-     ;; To begin with, after "module" or after "import" should be purple
-     ;; Note the SPACE below.
-     '("\\(module\\|import\\) \\([a-zA-Z0-9\-_\.]+\\)" 2 '((t (:foreground "purple"))))
-
-     ;; Agda special symbols: as
-     '(" as" 0 'agda2-highlight-symbol-face)
-
-     ;; Type, and constructor, names begin with a capital letter  --personal convention.
-     ;; They're preceded by either a space or an open delimiter character.
-     '("\\( \\|\s(\\)\\([A-Z]+\\)\\([a-zA-Z0-9\-_]*\\)" 0 'font-lock-type-face)
-     '("ℕ" 0 'font-lock-type-face)
-
-     ;; variables & function names, as a personal convention, begin with a lower case
-     '("\\([a-z]+\\)\\([a-zA-Z0-9\-_]*\\)" 0 '((t (:foreground "medium blue"))))
-
-     ;; colour numbers
-     '("\\([0-9]+\\)" 1   '((t (:foreground "purple"))))
-
-     ;; other faces to consider:
-     ;; 'font-lock-keyword-face 'font-lock-builtin-face 'font-lock-function-name-face
-     ;; 'font-lock-variable-name-face 'font-lock-constant-face
-     ))
-
-     ;; files that trigger this mode
-     nil
-
-     ;; any other functions to call
-     nil
-
-     ;; doc string
-     "My custom Agda highlighting mode for use *within* Org-mode."
-)
-
-(provide 'ob-agda-mode)
-
+;; [[file:~/org-agda-mode/literate.lagda::*(~lagda-to-org~)%20and%20(~org-to-lagda~)][(~lagda-to-org~) and (~org-to-lagda~):1]]
 (defun org-goto-line (line)
   "Go to the indicated line, unfolding the parent Org header.
 
@@ -92,7 +26,9 @@
   (org-cycle)
   (goto-line line)
   )
+;; (~lagda-to-org~) and (~org-to-lagda~):1 ends here
 
+;; [[file:~/org-agda-mode/literate.lagda::*(~lagda-to-org~)%20and%20(~org-to-lagda~)][(~lagda-to-org~) and (~org-to-lagda~):2]]
 (defun rewrite-ends (pre post new-pre new-post)
   "Perform the following in-buffer rewrite: ⟨pre⟩⋯⟨post⟩ ↦ ⟨newPre⟩⋯⟨newPost⟩.
   For example, for rewriting begin-end code blocks from Org-mode to something
@@ -101,15 +37,18 @@
   The search for the string ⟨pre⟩⋯⟨post⟩ is non-greedy, i.e. will find
   (in order) the minimal strings matching ⟨pre⟩⋯⟨post⟩.
 
+  We insist that the ends occur at the start of a newline; otherwise no
+  rewrite is made. Note the “^” regexp marker below.
+
   In the arguments, only symbol `\` needs to be escaped.
   "
-  (let ((rx-pre  (concat "\\(" (regexp-quote pre)  "\\)"))
-	(rx-post (concat "\\(" (regexp-quote post) "\\)"))
-	;; Code to match any characters (including newlines) based on https://www.emacswiki.org/emacs/MultilineRegexp
-	;; This version requires we end in a newline,
-	;; and uses the “non-greedy” * operator, *?, so we will match the minimal string.
-	(body "\\(.*\n\\)*?"))
-    (beginning-of-buffer)
+  (let ((rx-pre  (concat "\\(^" (regexp-quote pre)  "\\)"))
+        (rx-post (concat "\\(^" (regexp-quote post) "\\)"))
+        ;; Code to match any characters (including newlines) based on https://www.emacswiki.org/emacs/MultilineRegexp
+        ;; This version requires we end in a newline,
+        ;; and uses the “non-greedy” * operator, *?, so we will match the minimal string.
+        (body "\\(.*\n\\)*?"))
+    (goto-char (point-min))
     (while (re-search-forward (concat rx-pre body rx-post) nil t) ;; nil to search whole buffer, t to not error
       ;; Matched string 1 is the pre, matched string 3 is the post.
       ;; Optionals: fixed-case, literal, use buffer, substring
@@ -118,20 +57,22 @@
       )
     )
   )
+;; (~lagda-to-org~) and (~org-to-lagda~):2 ends here
 
+;; [[file:~/org-agda-mode/literate.lagda::*(~lagda-to-org~)%20and%20(~org-to-lagda~)][(~lagda-to-org~) and (~org-to-lagda~):3]]
 (defun lagda-to-org ()
   "Transform literate Agda blocks into Org-mode source blocks.
    Use haskell as the Org source block language since I do not have nice colouring otherwise.
   "
   (interactive)
   (let ((here-line (line-number-at-pos)) ;; remember current line
-	(here-column (current-column))
-	(enable-local-variables :safe)
-	)
-    (rewrite-ends "\n\\begin{code}"              "\n\\end{code}"
-		  "\n#+BEGIN_SRC org-agda"       "\n#+END_SRC")
-    (rewrite-ends "\n\\begin{spec}"              "\n\\end{spec}"
-		  "\n#+BEGIN_EXAMPLE org-agda"   "\n#+END_EXAMPLE")
+        (here-column (current-column))
+        (enable-local-variables :safe)
+        )
+    (rewrite-ends "\\begin{code}"              "\\end{code}"
+                  "#+BEGIN_SRC agda"       "#+END_SRC")
+    (rewrite-ends "\\begin{spec}"              "\\end{spec}"
+                  "#+BEGIN_EXAMPLE agda"   "#+END_EXAMPLE")
     (org-mode)
     (org-goto-line here-line) ;; defined above
     (move-to-column here-column)
@@ -145,13 +86,13 @@
   "
   (interactive)
   (let ((here-line (line-number-at-pos)) ;; remember current line
-	(here-column (current-column))  ;; and current column
-	(enable-local-variables :safe))
+        (here-column (current-column))  ;; and current column
+        (enable-local-variables :safe))
 
-    (rewrite-ends "\n#+BEGIN_SRC org-agda"       "\n#+END_SRC"
-		  "\n\\begin{code}"              "\n\\end{code}")
-    (rewrite-ends "\n#+BEGIN_EXAMPLE org-agda"   "\n#+END_EXAMPLE"
-		  "\n\\begin{spec}"              "\n\\end{spec}")
+    (rewrite-ends "#+BEGIN_SRC agda"       "#+END_SRC"
+                  "\\begin{code}"              "\\end{code}")
+    (rewrite-ends "#+BEGIN_EXAMPLE agda"   "#+END_EXAMPLE"
+                  "\\begin{spec}"              "\\end{spec}")
     (agda2-mode)
     (sit-for 0.1) ;; necessary for the slight delay between the agda2 commands
     (agda2-load)
@@ -160,9 +101,20 @@
   )
    (message "Welcome to Agda-mode, %s!" user-full-name)
 )
+;; (~lagda-to-org~) and (~org-to-lagda~):3 ends here
 
+;; [[file:~/org-agda-mode/literate.lagda::*(~lagda-to-org~)%20and%20(~org-to-lagda~)][(~lagda-to-org~) and (~org-to-lagda~):4]]
 (add-hook 'org-mode-hook
-	  (lambda () (local-set-key (kbd "C-x C-a") 'org-to-lagda)))
+          (lambda () (local-set-key (kbd "C-x C-a") 'org-to-lagda)))
 
 (add-hook 'agda2-mode-hook
-	  (lambda () (local-set-key (kbd "C-x C-a") 'lagda-to-org)))
+          (lambda ()
+            (local-set-key (kbd "C-x C-a") 'lagda-to-org)
+            (local-set-key (kbd "C-c C-v C-d")
+                           (lambda (prefix)
+                             (interactive "P") ;; Places value of universal argument into: current-prefix-arg
+                             (insert (if (identity current-prefix-arg)
+                                         "\n\\begin{spec}\n\n\\end{spec}"
+                                       "\n\\begin{code}\n\n\\end{code}"))
+                             (forward-line -1)))))
+;; (~lagda-to-org~) and (~org-to-lagda~):4 ends here
